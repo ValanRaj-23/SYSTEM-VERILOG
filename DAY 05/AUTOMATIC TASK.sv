a69466e23ecceb24ad58747a55bbafd2d83e1135
ValanRaj-23 <valanrajs.2364@gmail.com>
2025-02-22 22:36:20 +0530

    AUTOMATIC TASK.sv

---
+++ DAY 05/AUTOMATIC TASK.sv
@@ -0,0 +1,25 @@
+module automatic_task;
+  int data1 , data2;
+  
+   task automatic increment();
+    int i;
+     data1 =i++;
+    $display("Incremented value : %0d", data1);
+  endtask
+  
+  task increment_past();
+    int i;
+     data1 =i++;
+    $display("Incremented value : %0d", data1);
+  endtask
+  
+  initial begin
+    repeat(5)begin
+      increment();
+    end
+    
+    repeat(5)begin
+      increment_past();
+    end
+  end
+ endmodule
